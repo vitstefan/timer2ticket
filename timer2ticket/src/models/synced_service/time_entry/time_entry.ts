2ea80c5cecc2976f0184344dd35490660bb2a8e4
vitstefan <68771387+vitstefan@users.noreply.github.com>
2021-04-25 18:22:41 +0200

    Delete timer2ticket directory

--- timer2ticket/src/models/synced_service/time_entry/time_entry.ts
+++
@@ -1,9 +0,0 @@
-export interface TimeEntry {
-  id: number | string;
-  projectId: number | string;
-  text: string;
-  start: Date | number | string;
-  end: Date | number | string;
-  durationInMilliseconds: number;
-  lastUpdated: Date | number | string;
-}
--- timer2ticket/src/shared/constants.ts
+++
@@ -1,5 +0,0 @@
-export class Constants {
-  static appPort = 3000;
-
-  static mongoDbUrl = 'mongodb://localhost:27017';
-}
