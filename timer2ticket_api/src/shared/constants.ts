bd66fd2c1e17fcb970c94f4f80124d2068bfb42c
vitstefan <68771387+vitstefan@users.noreply.github.com>
2021-04-25 18:22:50 +0200

    Delete timer2ticket_api directory

--- timer2ticket_api/src/shared/constants.ts
+++
@@ -1,9 +0,0 @@
-export class Constants {
-  static appPort = 3001;
-
-  static t2tCoreApiUrl = 'http://localhost:3000/api/';
-
-  static mongoDbUrl = 'mongodb://localhost:27017';
-
-  static bcryptSaltRounds = 10;
-}
